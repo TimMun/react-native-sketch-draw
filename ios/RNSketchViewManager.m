76ad3ce38d91e4a3633901ebe6ae1ce34a8dc299
Tim Mun <tmun@berkeley.edu>
2018-06-26 15:53:57 -0700

    Fix SketchViewContainer resizing bug

--- ios/RNSketchViewManager.m
+++ ios/RNSketchViewManager.m
@@ -9,25 +9,25 @@
 
 RCT_CUSTOM_VIEW_PROPERTY(selectedTool, NSInteger, SketchViewContainer)
 {
-    SketchViewContainer *currentView = !view ? defaultView : view;
+    SketchViewContainer *currentView = !view ? defaultView.subviews[0] : view.subviews[0];
     [currentView.sketchView setToolType:[RCTConvert NSInteger:json]];
 }
 
 RCT_CUSTOM_VIEW_PROPERTY(toolColor, UIColor, SketchViewContainer)
 {
-    SketchViewContainer *currentView = !view ? defaultView : view;
+    SketchViewContainer *currentView = !view ? defaultView.subviews[0] : view.subviews[0];
     [currentView.sketchView setToolColor:[RCTConvert UIColor:json]];
 }
 
 RCT_CUSTOM_VIEW_PROPERTY(toolThickness, NSInteger, SketchViewContainer)
 {
-    SketchViewContainer *currentView = !view ? defaultView : view;
+    SketchViewContainer *currentView = !view ? defaultView.subviews[0] : view.subviews[0];
     [currentView.sketchView setToolThickness:[RCTConvert NSInteger:json]];
 }
 
 RCT_CUSTOM_VIEW_PROPERTY(localSourceImagePath, NSString, SketchViewContainer)
 {
-    SketchViewContainer *currentView = !view ? defaultView : view;
+    SketchViewContainer *currentView = !view ? defaultView.subviews[0] : view.subviews[0];
     NSString *localFilePath = [RCTConvert NSString:json];
     dispatch_async(dispatch_get_main_queue(), ^{
         [currentView openSketchFile:localFilePath];
@@ -38,8 +38,13 @@ RCT_EXPORT_MODULE(RNSketchView)
 
 -(UIView *)view
 {
+    // Must wrap in parent view since RN messes up the frame: https://github.com/facebook/react-native/issues/2948
     self.sketchViewContainer = [[[NSBundle mainBundle] loadNibNamed:@"SketchViewContainer" owner:self options:nil] firstObject];
-    return self.sketchViewContainer;
+    UIView *parent = [[UIView alloc]initWithFrame:self.sketchViewContainer.bounds];
+    [self.sketchViewContainer setBackgroundColor:[UIColor clearColor]];
+    [parent setBackgroundColor:[UIColor clearColor]];
+    [parent addSubview:self.sketchViewContainer];
+    return parent;
 }
 
 RCT_EXPORT_METHOD(saveSketch:(nonnull NSNumber *)reactTag) {
